ed24bfe5ed7a74d2b79a3c49acace0127a43acb4
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-08-24 12:27:44 +0530

    Created Routes

---
+++ src/components/GetLicence.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GetLicence() {
+  return (
+    <div>GetLicense</div>
+  )
+}
+
+export default GetLicence
---
+++ src/pages/PriceList/PricesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PricesList() {
+  return (
+    <div>PricesList</div>
+  )
+}
+
+export default PricesList
