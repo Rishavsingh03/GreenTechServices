143320eb62f4646228aa59443ac6fde6b594a77f
Rishavsingh03 <rishavkumar3864@gmail.com>
2024-08-24 13:06:43 +0530

    Created Login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../Redux/authActions";
+import "./login.css";
+import { useNavigate } from "react-router-dom";
 
-function login() {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate =useNavigate();
+  const dispatch = useDispatch();
+  const { user,role,loading, error } = useSelector((state) => state.auth);
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    await dispatch(loginUser(email, password));
+    await console.log("user",user);
+    await console.log("role", role);
+    navigate("/");
+  };
+
   return (
-    <div>login</div>
-  )
-}
+    <div className="login-container d-flex justify-content-center align-items-center vh-100">
+      <div className="login-box p-5 rounded">
+        <h2 className="text-center mb-4">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group mb-4">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {error && <div className="alert alert-danger">{error}</div>}
+          <button
+            type="submit"
+            className="btn btn-primary btn-block w-100"
+            disabled={loading}
+          >
+            {loading ? "Logging in..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default login
+export default Login;
